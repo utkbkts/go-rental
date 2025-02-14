321818ab375ec667fafb5d72c82c2039efef163f
utku <utkbktss5@gmail.com>
2025-02-14 21:44:36 +0300

    [feat]: Add header with avatar dropdown menu

---
+++ frontend/src/components/layout/header/Header.tsx
@@ -0,0 +1,59 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import MobileMenu from "./partials/MobileMenu";
+import { FaCar } from "react-icons/fa"; // Örnek ikon
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+const Header = () => {
+
+
+  return (
+    <div className="shadow-md w-full py-4  bg-white dark:bg-gray-800 transition-colors duration-300">
+      <div className="flex items-center justify-between container mx-auto">
+        <h1 className="text-2xl flex items-center gap-2 font-semibold text-gray-800 dark:text-white">
+          <FaCar className="text-blue-500 mt-1" />
+          Car{" "}
+          <span className="text-blue-400 font-bold underline-offset-2 underline">
+            Go
+          </span>
+        </h1>
+        <div className="hidden md:flex items-center gap-4">
+          <Button className="cursor-pointer hover:bg-blue-500 hover:text-white transition duration-300">
+            Login
+          </Button>
+          <div className="relative">
+            {/* Avatar with Menu */}
+            {/* Dropdown Menu */}
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Avatar className="cursor-pointer">
+                  <AvatarImage src="https://github.com/shadcn.png" />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem >Profile</DropdownMenuItem>
+                <DropdownMenuItem>Billing</DropdownMenuItem>
+                <DropdownMenuItem>Team</DropdownMenuItem>
+                <DropdownMenuItem>Subscription</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+        <div className="md:hidden flex items-center justify-end">
+          <MobileMenu />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ frontend/src/components/layout/header/partials/MobileMenu.tsx
@@ -0,0 +1,41 @@
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { FaBars } from "react-icons/fa";
+
+const MobileMenu = () => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button className="cursor-pointer" variant="outline"><FaBars /></Button>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Edit profile</SheetTitle>
+          <SheetDescription>
+            Make changes to your profile here. Click save when you're done.
+          </SheetDescription>
+        </SheetHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4"></div>
+          <div className="grid grid-cols-4 items-center gap-4"></div>
+        </div>
+        <SheetFooter>
+          <SheetClose asChild>
+            <Button type="submit">Save changes</Button>
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileMenu;
